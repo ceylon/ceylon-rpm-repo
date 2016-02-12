554a30068a0d50892a8bbc30b0c023cc2822a08f
Tako Schotanus <tako@ceylon-lang.org>
2016-02-12 01:36:09 +0100

    Updated list of RPMs for repo

--- repo/build.sh
+++ repo/build.sh
@@ -4,7 +4,7 @@ RPMS="ceylon-1.0.0-1.0.0-0.noarch.rpm \
  ceylon-1.1.0-1.1.0-0.noarch.rpm \
  ceylon-1.1.0-1.1.0-1.noarch.rpm \
  ceylon-1.2.0-1.2.0-0.noarch.rpm \
- ceylon-1.2.1-1.2.1-0.noarch.rpm" 
+ ceylon-1.2.1-1.2.1-1.noarch.rpm" 
 rm -rf repodata
 
 for pkg in $RPMS
