f38e2331c86c28dee024b6a7bb1e6d02942a89a4
Tako Schotanus <tako@ceylon-lang.org>
2016-03-08 14:31:31 +0100

    Added version 1.2.2

--- repo/build.sh
+++ repo/build.sh
@@ -6,8 +6,8 @@ RPMS="ceylon-1.0.0-1.0.0-0.noarch.rpm \
  ceylon-1.1.0-1.1.0-0.noarch.rpm \
  ceylon-1.1.0-1.1.0-1.noarch.rpm \
  ceylon-1.2.0-1.2.0-0.noarch.rpm \
- ceylon-1.2.1-1.2.1-0.noarch.rpm \
- ceylon-1.2.1-1.2.1-1.noarch.rpm" 
+ ceylon-1.2.1-1.2.1-1.noarch.rpm
+ ceylon-1.2.2-1.2.2-0.noarch.rpm" 
 rm -rf repodata
 
 for pkg in $RPMS
