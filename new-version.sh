121502f28145afbb41cdad787bb370f6b7688e92
Stephane Epardaud <stef@epardaud.fr>
2017-04-27 16:49:36 +0200

    Fixes

--- new-version.sh
+++ new-version.sh
@@ -23,11 +23,11 @@ CHDATE=`date "+* %a %b %d %Y"`
 CHAUTHOR="Stephane Epardaud <stef@epardaud.fr>"
 
 perl -pi -e "s/(#.*\@NEW_VERSION\@)/RPMS=\"\\\$RPMS ceylon-${VERSION}_${VERSION}-0.noarch.rpm\"\n\$1/" repo/build.sh
-perl -pi -e "s/^(%changelog)$/\$1\n$CHDATE $CHAUTHOR ${VERSION}-0/" dist-pkg/ceylon.spec
-perl -pi -e "s/^(%major_version) .*$/\$1 ${VS[0]}/" dist-pkg/ceylon.spec
-perl -pi -e "s/^(%minor_version) .*$/\$1 ${VS[1]}/" dist-pkg/ceylon.spec
-perl -pi -e "s/^(%micro_version) .*$/\$1 ${VS[2]}/" dist-pkg/ceylon.spec
-perl -pi -e "s/^(%alternatives_version) .*$/\$1 ${PRIO}/" dist-pkg/ceylon.spec
+perl -pi -e "s/^(%changelog)$/\$1\n$CHDATE $CHAUTHOR ${VERSION}-0\n- New version ${VERSION}/" dist-pkg/ceylon.spec
+perl -pi -e "s/^(%define major_version) .*$/\$1 ${VS[0]}/" dist-pkg/ceylon.spec
+perl -pi -e "s/^(%define minor_version) .*$/\$1 ${VS[1]}/" dist-pkg/ceylon.spec
+perl -pi -e "s/^(%define micro_version) .*$/\$1 ${VS[2]}/" dist-pkg/ceylon.spec
+perl -pi -e "s/^(%define alternatives_version) .*$/\$1 ${PRIO}/" dist-pkg/ceylon.spec
 git commit -a -m "New version $VERSION"
 
 git checkout -q -b version-${VERSION}
