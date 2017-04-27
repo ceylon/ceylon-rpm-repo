3b47df32ad55526233b0252605719a87b5b54263
Stephane Epardaud <stef@epardaud.fr>
2017-04-27 16:47:11 +0200

    New script for automated release

---
+++ new-version.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [[ $# != 1 ]]; then
+    echo "Usage: $0 <version>"
+    echo ""
+    echo "eg: $0 1.2.1"
+    exit
+fi
+
+VERSION=$1
+
+VS=(${VERSION//\./ })
+if [[ ${#VS[@]} != 3 ]]; then
+    echo "Error: version must have 3 numbers separated by periods, eg 1.2.1"
+    exit
+fi
+
+PRIO=$(printf "%d%02d%02d0" ${VS[0]} ${VS[1]} ${VS[2]})
+
+echo "Using version $VERSION and priority $PRIO..."
+
+CHDATE=`date "+* %a %b %d %Y"`
+CHAUTHOR="Stephane Epardaud <stef@epardaud.fr>"
+
+perl -pi -e "s/(#.*\@NEW_VERSION\@)/RPMS=\"\\\$RPMS ceylon-${VERSION}_${VERSION}-0.noarch.rpm\"\n\$1/" repo/build.sh
+perl -pi -e "s/^(%changelog)$/\$1\n$CHDATE $CHAUTHOR ${VERSION}-0/" dist-pkg/ceylon.spec
+perl -pi -e "s/^(%major_version) .*$/\$1 ${VS[0]}/" dist-pkg/ceylon.spec
+perl -pi -e "s/^(%minor_version) .*$/\$1 ${VS[1]}/" dist-pkg/ceylon.spec
+perl -pi -e "s/^(%micro_version) .*$/\$1 ${VS[2]}/" dist-pkg/ceylon.spec
+perl -pi -e "s/^(%alternatives_version) .*$/\$1 ${PRIO}/" dist-pkg/ceylon.spec
+git commit -a -m "New version $VERSION"
+
+git checkout -q -b version-${VERSION}
+
+git push --set-upstream origin version-$VERSION
+git tag $VERSION
+git push origin $VERSION
+
--- repo/build.sh
+++ repo/build.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-RPMS="ceylon-1.0.0-1.0.0-0.noarch.rpm \
- ceylon-1.1.0-1.1.0-0.noarch.rpm \
- ceylon-1.1.0-1.1.0-1.noarch.rpm \
- ceylon-1.2.0-1.2.0-0.noarch.rpm \
- ceylon-1.2.1-1.2.1-1.noarch.rpm \
- ceylon-1.2.2-1.2.2-0.noarch.rpm \
- ceylon-1.3.0-1.3.0-0.noarch.rpm \
- ceylon-1.3.1-1.3.1-1.noarch.rpm \
- ceylon-1.3.2-1.3.2-0.noarch.rpm"
+RPMS="ceylon-1.0.0-1.0.0-0.noarch.rpm"
+RPMS="$RPMS ceylon-1.1.0-1.1.0-0.noarch.rpm"
+RPMS="$RPMS ceylon-1.1.0-1.1.0-1.noarch.rpm"
+RPMS="$RPMS ceylon-1.2.0-1.2.0-0.noarch.rpm"
+RPMS="$RPMS ceylon-1.2.1-1.2.1-1.noarch.rpm"
+RPMS="$RPMS ceylon-1.2.2-1.2.2-0.noarch.rpm"
+RPMS="$RPMS ceylon-1.3.0-1.3.0-0.noarch.rpm"
+RPMS="$RPMS ceylon-1.3.1-1.3.1-1.noarch.rpm"
+RPMS="$RPMS ceylon-1.3.2-1.3.2-0.noarch.rpm"
+# @NEW_VERSION@
 rm -rf repodata
 
 for pkg in $RPMS
